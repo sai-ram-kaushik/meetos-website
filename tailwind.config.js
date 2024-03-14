c759e3b6a411a96515f51180925180c6e07638a9
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-03-15 00:23:51 +0530

    updated and initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#000000",
+        primary: "#FFFFFF",
+        secondary: "#FFA500"
+      }
+    },
+  },
+  plugins: [],
+}
+
